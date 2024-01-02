4a0d65a1bfde830fa84714d0a85e122e63471ef8
Alec LaLonde <alec@boxelderweb.com>
2024-01-02 16:38:48 -0700

    Generated models / api from App Store Connect version 3.1

--- gen-openapi.sh
+++ gen-openapi.sh
@@ -1,5 +1,5 @@
 npx openapi-generator-cli generate \
-  -i ./app_store_connect_api_2.2_openapi.json \
+  -i ./local/app_store_connect_api_3.1_openapi.json \
   -o src/openapi \
   -g typescript-fetch \
   --additional-properties=supportsES6=true \
