1f7961245b84b7fe10ecc57d507b6552bfafe7d8
Alec LaLonde <alec@boxelderweb.com>
2024-01-02 17:15:58 -0700

    Added availability api/models from v2.3

--- gen-openapi.sh
+++ gen-openapi.sh
@@ -1,6 +1,6 @@
 npx openapi-generator-cli generate \
-  -i ./app_store_connect_api_2.2_openapi.json \
-  -o src/openapi \
+  -i ./app_store_connect_api_2.3_openapi.json \
+  -o src/openapi2 \
   -g typescript-fetch \
   --additional-properties=supportsES6=true \
   --skip-validate-spec
